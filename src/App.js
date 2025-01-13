96f04e906b66af8fa6993c9869ba2bd7a767f8fc
Adeola-Web2 <adeolafayinminu@loyalistcollege.com>
2025-01-13 13:20:52 -0500

    Added my name to App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, Adeola
         </a>
       </header>
     </div>
